9dcdf4f8084f49abb7f5c392e8827f97f2dac5ad
agvolodin <agvolodin@stdu.info>
2024-10-23 20:50:41 +0400

    clear template

---
+++ HW1/task02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (int argc, char **argv) 
+{
+    printf("\n(\\___/)\n");
+    printf("(='.'=)\n");
+    printf("(\")_(\")\n\n");
+    printf("Привет мир!\n");
+    return 0;
+}
