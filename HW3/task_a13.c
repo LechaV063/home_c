5ed96ce8485a099ff5a1d2b1e1188f480d10e7b8
agvolodin <agvolodin@stdu.info>
2024-11-04 18:38:39 +0400

    make HW3

---
+++ HW3/task_a13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int digit_1=0,digit_2=0,digit_3=0;    
+    int sumOfDigit;
+    int number;
+    scanf("%d", &number);
+    digit_1 = number / 100;
+    digit_2 = (number - digit_1 * 100)/10;
+    digit_3 = (number - digit_1 * 100 - digit_2 * 10);
+    printf("%d\n", digit_1*digit_2*digit_3);
+}
