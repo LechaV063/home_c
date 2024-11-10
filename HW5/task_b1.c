af6fba5ddcf84748edf4e0fecc586fd1cd14cb90
agvolodin <agvolodin@stdu.info>
2024-11-10 21:37:48 +0400

    add comment to B1

--- HW5/task_b1.c
+++ HW5/task_b1.c
@@ -3,9 +3,9 @@
 int main(int argv, char ** argc) {
     int n;
     scanf("%d", &n);
-    if (n<=100) {
+    if (n<=100) {  // число должно быть меньше 100 по условию
     for (int i=1;i<=n;i++) {
-        printf("%d %d %d\n", i, i*i, i*i*i);
+        printf("%d %d %d\n", i, i*i, i*i*i); // вывод текущего числа и его квадрата и куба
         }
     }
     return 0;
