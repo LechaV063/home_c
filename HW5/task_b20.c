13dba8ff017aa0fb788fb064b46d729b4d22e7f6
agvolodin <agvolodin@stdu.info>
2024-11-12 23:24:18 +0400

    add task B20

--- HW5/task_b20.c
+++ HW5/task_b20.c
@@ -4,4 +4,19 @@
 
 int main(int argv, char **argc)
 {
+    int userNumber, simple=1;
+    scanf("%d", &userNumber);
+    if(userNumber == 1) {
+        printf("NO\n");
+        return 0;
+    }
+    for (int i=2; i<userNumber; i++) {
+        if(userNumber%i == 0 && userNumber/i >0)
+        {
+            simple = 0;
+            break;
+        } 
+    }
+    simple ? printf("YES\n") : printf("NO\n");
+    return 0;
 }
