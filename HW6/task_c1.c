575e979b26e1f32f94496cc41ee996f27486bfad
agvolodin <agvolodin@stdu.info>
2024-11-14 00:20:10 +0400

    add tasks C1 C2 C3

--- HW6/task_c1.c
+++ HW6/task_c1.c
@@ -2,7 +2,16 @@
 
 //  Составить функцию, модуль числа и привести пример ее использования.
 
-int main(int argv, char ** argc) {
+int absolute(int num);
 
+int main(int argv, char ** argc) {
+    int userNumber;
+    scanf("%d", &userNumber);
+    printf("%d\n", absolute(userNumber));
     return 0;
+}
+
+int absolute (int num) {
+    int result = num < 0 ? -1 * num :  num;
+    return result;
 }
--- HW6/task_c2.c
+++ HW6/task_c2.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
-//  Составить функцию, возведение числа N в степень P. 
+//  Составить функцию, возведение числа N в степень P.
 // int power(n, p) и привести пример ее использования.
 
-int main(int argv, char ** argc) {
-    
+int power(int n, int p);
+
+int main(int argv, char **argc)
+{
+    int N, P;
+    scanf("%d %d", &N, &P);
+    if (N <= 1000 && P <= 1000 && P >= 0) // Проверка корректности ввода
+    {
+        printf("%d\n", power(N, P));
+    }
     return 0;
 }
+
+int power(int n, int p)
+{
+    int result = 1; // для степени 0
+    if (p > 1)
+    {
+        for (int i = 1; i <= p; i++)
+        {
+            result *= n;
+        }
+    }
+    return result;
+};
