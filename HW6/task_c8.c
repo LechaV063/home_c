b20703ea69f839a94cbe47fb3ebc561237dc2e0c
agvolodin <agvolodin@stdu.info>
2024-11-13 21:07:23 +0400

    add definition for tasks 1 ..8

--- HW6/task_c8.c
+++ HW6/task_c8.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-//  
-// 
+//  Составить функцию вычисления N!. 
+// Использовать ее при вычислении факториала
+// int factorial(int n)
 
 int main(int argv, char ** argc) {
     
