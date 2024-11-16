a02058600121b8766e8e385f9707bf1655c3965d
agvolodin <agvolodin@stdu.info>
2024-11-16 13:03:47 +0400

    C10

--- HW6/task_c10.c
+++ HW6/task_c10.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
 
-// Составить функцию, печать всех простых множителей числа. 
+// Составить функцию, печать всех простых множителей числа.
 // Использовать ее для печати всех простых множителей числа.
-// void print_simple(int n)
 
-int main(int argv, char ** argc) {
-    
+void print_simple(int n);
+
+int main(int argv, char **argc)
+{
+    int userNumber;
+    scanf("%d", &userNumber);
+    print_simple(userNumber);
     return 0;
+}
+
+void print_simple(int n)
+{
+    int i = 2;
+    if (n == 0)
+    {
+        printf("0\n");
+        return;
+    }
+    if (n == 1)
+    {
+        printf("1\n");
+        return;
+    }
+    while (n > 1)
+    {
+        if (n % i == 0)
+        {
+            printf("%d ", i);
+            n /= i;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    printf("\n");
+    return;
 }
