47a26abb5105103d8b3eaf7afc8005a9d39854b5
agvolodin <agvolodin@stdu.info>
2024-11-16 15:33:46 +0400

    add C12 C13 and final result screen

--- HW6/task_c12.c
+++ HW6/task_c12.c
@@ -2,9 +2,53 @@
 
 //  Составить функцию, которая вычисляет синус как сумму ряда (с точностью 0.001)
 // sin(x) = x - x3/3! + x5/5! - x7/7! + ...(x в радианах)
-// float sinus(float x)
+
+float sinus(float x);
+float power(float n, int p);
+int factorial(int n);
 
 int main(int argv, char ** argc) {
-    
+    float pi=3.14159;
+    int deg;
+    float xRadians;
+    scanf("%d", &deg);
+    if (deg >=0 && deg <=90) {
+        xRadians = deg * pi / 180;
+        printf("%.3f\n",sinus(xRadians));
+    }
     return 0;
+}
+
+float power(float n, int p)
+{
+    float result = 1; // для степени 0
+    if (p > 1)
+    {
+        for (int i = 1; i <= p; i++)
+        {
+            result *= n;
+        }
+    }
+    return result;
+};
+
+int factorial(int n)
+{
+    unsigned int result = 1;
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+    }
+    return result;
+}
+
+float sinus(float x) {
+    return x - power(x,3)/factorial(3) + power(x,5)/factorial(5) - power(x,7)/factorial(7);
 }
--- HW6/task_c13.c
+++ HW6/task_c13.c
@@ -4,7 +4,54 @@
 // cos(x) = 1 - x2/2! + x4/4! - x6/6! + ... (x в радианах)
 // float cosinus(float x)
 
-int main(int argv, char ** argc) {
-    
+float cosinus(float x);
+float power(float n, int p);
+int factorial(int n);
+
+int main(int argv, char **argc)
+{
+    float pi=3.14159;
+    int deg;
+    float xRadians;
+    scanf("%d", &deg);
+    if (deg >=0 && deg <=90) {
+        xRadians = deg * pi / 180;
+        printf("%.3f\n",cosinus(xRadians));
+    }
     return 0;
+}
+
+float power(float n, int p)
+{
+    float result = 1; // для степени 0
+    if (p > 1)
+    {
+        for (int i = 1; i <= p; i++)
+        {
+            result *= n;
+        }
+    }
+    return result;
+};
+
+int factorial(int n)
+{
+    unsigned int result = 1;
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+    }
+    return result;
+}
+
+float cosinus(float x)
+{
+    return 1 - power(x, 2) / factorial(2) + power(x, 4) / factorial(4) - power(x, 6) / factorial(6);
 }
