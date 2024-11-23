c7e43ff6c0f609fcb7f06e734c013e66d4489aef
agvolodin <agvolodin@stdu.info>
2024-11-23 12:26:23 +0400

    E2 E3

---
+++ HW8/task_E2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Постановка задачи:
+// Ввести c клавиатуры массив из 5 элементов, найти минимальный из них.
+// Входные данные: 5 целых чисел через пробел
+// Выходные данные: Одно единственное целое число
+
+const int LENGTH = 5;
+int inputData(int *arr, int n);
+int minOfArray(int *arr, int n);
+
+int main(int argv, char **argc)
+{
+    int inptArray[LENGTH];
+    inputData(inptArray, LENGTH);
+    printf("%d\n", minOfArray(inptArray, LENGTH));
+    return 0;
+}
+
+int inputData(int *arr, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    return i;
+};
+
+int minOfArray(int *arr, int n)
+{
+    int minimum = arr[0];
+    for (size_t i = 0; i < n; i++)
+    {
+        minimum = arr[i]<minimum?arr[i]:minimum;
+    }
+    return minimum;
+}
---
+++ HW8/task_E3.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+// Постановка задачи:
+// Считать массив из 10 элементов и найти в нем максимальный и минимальный элементы и их номера.
+// Входные данные: 10 целых чисел через пробел
+// Выходные данные: 4 целых числа через пробел: номер максимума, максимум, номер минимума, минимум.
+
+const int LENGTH = 10; // длина массива
+int inputData(int *arr, int n); // для заполнения массива, возращает индекс последнего элемента
+int minOfArray(int *arr, int n, int *index); // возвращает минимум из массива arr  *index - указатель на индекс результата
+int maxOfArray(int *arr, int n, int *index); // возвращает максимум из массива arr *index - указатель на индекс результата
+
+int main(int argv, char **argc)
+{
+    int inptArray[LENGTH];
+    int indexOfMax = 0, indexOfMin = 0; // начальные условия
+    int max, min;
+    inputData(inptArray, LENGTH);
+    min = minOfArray(inptArray, LENGTH, &indexOfMin);
+    max = maxOfArray(inptArray, LENGTH, &indexOfMax);
+    printf("%d %d %d %d\n", indexOfMax + 1, max, indexOfMin + 1, min);
+    return 0;
+}
+
+int inputData(int *arr, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    return i;
+};
+
+int minOfArray(int *arr, int n, int *index)
+{
+    int minimum = arr[0];
+    *index = 0;
+    for (size_t i = 0; i < n; i++)
+    {
+        if (arr[i] < minimum)
+        {
+            minimum = arr[i];
+            *index = i;
+        }
+    }
+    return minimum;
+}
+
+int maxOfArray(int *arr, int n, int *index)
+{
+    int maximum = arr[0];
+    for (size_t i = 0; i < n; i++)
+    {
+        if (arr[i] > maximum)
+        {
+            maximum = arr[i];
+            *index = i;
+        }
+    }
+    return maximum;
+}
