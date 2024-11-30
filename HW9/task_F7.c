112d0cc8d88c9735635a578fb6b8144a6864e559
agvolodin <agvolodin@stdu.info>
2024-11-30 23:45:57 +0400

    F7

--- HW9/task_F7.c
+++ HW9/task_F7.c
@@ -9,8 +9,60 @@
 // Входныне данные: Функция принимает исходный массив a[] и сжимает в массив b[], N - число элементов в массиве a[].
 // Выходные данные: Функция возвращает число элементов сжатого массива b[]
 
-int main(int argv, char **argc)
+int compression(int a[], int b[], int N)
 {
-
-    return 0;
+    int currentBit = 0;
+    int count = 0, j = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (currentBit == a[i])
+        {
+            count++;
+            b[j]=count;
+        }
+        else
+        {
+            b[j] = count;
+            j++;
+            count = 1;
+            b[j]=count;
+            currentBit = a[i];
+        }
+    }
+    return j+1;
 }
+
+// int inputData(int n, int *arr)
+// {
+//     int i;
+//     for (i = 0; i < n; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//     }
+//     return i;
+// };
+
+// void printData(int n, int *arr)
+// {
+//     int i;
+//     for (i = 0; i < n; i++)
+//     {
+//         printf("%d", arr[i]);
+//         if (i != n - 1) // после последнего элемента пробел не пишим
+//         {
+//             putchar(' ');
+//         }
+//     }
+//     putchar('\n');
+// };
+
+// #define NUMS 100
+
+// int main(int argv, char **argc)
+// {
+//     int size, i = 0, arrA[NUMS], arrB[NUMS], len;
+//     inputData(NUMS, arrA);
+//     len = compression(arrA, arrB, NUMS);
+//     printData(len, arrB);
+//     return 0;
+// }
