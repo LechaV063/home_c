828d29fbdd13a71c5e2c1e192291014a25356b40
agvolodin <agvolodin@stdu.info>
2025-01-01 21:03:49 +0400

    CONTEST-G tasks G16 and G15

--- HW10/task_G15.c
+++ HW10/task_G15.c
@@ -1,30 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Постановка задачи:
-// В файле input.txt дано предложение. Необходимо заменить 
+// В файле input.txt дано предложение. Необходимо заменить
 // все имена «Cao» на «Ling» и записать результат в файл output.txt.
 // Входные данные: Строка из английский букв, знаков препинания и пробелов. Не более 1000 символов.
 // Выходные данные: Строка из английский букв, знаков препинания и пробелов.
-
 
 #define MAXLEN 1000
 
 const char inputFileName[] = "input.txt";
 const char outputFileName[] = "output.txt";
+void modifyStr(char *inStr, char *outStr, char *dst, char *src)
+{
+    char *pTmp, *pInput = inStr;
+    int numbers, lenDst = strlen(dst), lenSrc = strlen(src);
+    while ((pTmp = strstr(pInput, src)) != NULL)
+    {
+        numbers = pTmp - pInput;
+        strncat(outStr, pInput, numbers);
+        strcat(outStr, dst);
+        pInput = pTmp + lenSrc;
+    };
+    strcat(outStr, pInput);
+};
 
 int main(int argv, char **argc)
 {
     FILE *fpIn, *fpOut;
     fpIn = fopen(inputFileName, "r");
     fpOut = fopen(outputFileName, "w");
+    char srcSubStrng[] = "Cao", dstSubStrng[] = "Ling";
+    char inpString[MAXLEN] = {0}, outString[MAXLEN+250] = {0};
     if (fpIn == NULL || fpOut == NULL) // проверка на ошибку открытия файлов
     {
         printf("Файл %s не найден! Аварийный выход из программы.\n", inputFileName);
         exit(1);
     }
+    fscanf(fpIn, "%[^\n]", inpString);
+    // printf("%s", inpString);
+    modifyStr(inpString, outString, dstSubStrng, srcSubStrng);
+    fprintf(fpOut, "%s", outString);
     fclose(fpOut);
     fclose(fpIn);
     return 0;
 }
-
