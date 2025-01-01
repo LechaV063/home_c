f8ce34b871751411dd8db97f5eb0959d6ca2bbd3
agvolodin <agvolodin@stdu.info>
2025-01-02 02:04:37 +0400

    CONTEST-G task G19 G20

--- HW10/task_G19.c
+++ HW10/task_G19.c
@@ -9,24 +9,78 @@
 // Выходные данные: Строка из маленьких английских букв.
 
 #define MAXLEN 1000
+#define NUMBLETTERS 26
+typedef struct
+{
+    char symbol;
+    int freq;
+} letter;
 
 const char inputFileName[] = "input.txt";
 const char outputFileName[] = "output.txt";
+
+void poliandrom(char *inpS, char *outS)
+{
+    letter arrLetters[NUMBLETTERS];
+    int lenOutputWord = 0;
+    int indxLetter = 0, indxOutpWord = 0;
+    int poliandromPossible = 0;
+    int additionLetter = 0;
+    for (size_t i = 0; i < NUMBLETTERS; i++)
+    {
+        arrLetters[i].symbol = 'a' + i;
+        arrLetters[i].freq = 0;
+    }
+    for (size_t i = 0; i < strlen(inpS); i++)
+    {
+        if (inpS[i] >= 'a' && inpS[i] <= 'z')
+        {
+            indxLetter = inpS[i] - 'a';
+            arrLetters[indxLetter].freq++;
+        }
+    }
+    for (size_t i = 0; i < NUMBLETTERS; i++)
+    {
+        while (arrLetters[i].freq >= 2)
+        {
+            outS[indxOutpWord] = arrLetters[i].symbol;
+            lenOutputWord++;
+            indxOutpWord++;
+            arrLetters[i].freq -= 2;
+            poliandromPossible = 1;
+        }
+    }
+    lenOutputWord *= 2;
+    for (size_t i = 0; i < NUMBLETTERS; i++)
+    {
+        if (arrLetters[i].freq > 0)
+        {
+            outS[indxOutpWord] = arrLetters[i].symbol;
+            additionLetter = 1;
+            break;
+        }
+    }
+    for (size_t i = 0; i < (lenOutputWord + additionLetter - 1) / 2; i++)
+    {
+        outS[lenOutputWord - i] = outS[i];
+    }
+    // printf("%s\n", outS);
+}
 
 int main(int argv, char **argc)
 {
     FILE *fpIn, *fpOut;
     fpIn = fopen(inputFileName, "r");
     fpOut = fopen(outputFileName, "w");
-    char inpString[MAXLEN], outString[MAXLEN];
+    char inpString[MAXLEN] = {0}, outString[MAXLEN] = {0};
     if (fpIn == NULL || fpOut == NULL) // проверка на ошибку открытия файлов
     {
         printf("Файл %s не найден! Аварийный выход из программы.\n", inputFileName);
         exit(1);
     }
     fscanf(fpIn, "%[^\n]", inpString);
-    printf("%s", inpString);
-    
+    poliandrom(inpString, outString);
+    fprintf(fpOut, "%s", outString);
     fclose(fpOut);
     fclose(fpIn);
     return 0;
--- HW10/task_G20.c
+++ HW10/task_G20.c
@@ -10,24 +10,76 @@
 // Выходные данные: YES или NO
 
 #define MAXLEN 1000
+#define NUMBLETTERS 26
+typedef struct
+{
+    char symbol;
+    int freq;
+} letter;
 
 const char inputFileName[] = "input.txt";
 const char outputFileName[] = "output.txt";
-int is_palindrom(string);
+
+int is_palindrom(char *inpS)
+{
+    letter arrLetters[NUMBLETTERS];
+    int result = 0;
+    int indxLetter;
+    for (size_t i = 0; i < NUMBLETTERS; i++)
+    {
+        arrLetters[i].symbol = 'a' + i;
+        arrLetters[i].freq = 0;
+    }
+    for (size_t i = 0; i < strlen(inpS); i++)
+    {
+        if (inpS[i] >= 'a' && inpS[i] <= 'z')
+        {
+            indxLetter = inpS[i] - 'a';
+            arrLetters[indxLetter].freq++;
+        }
+    }
+    for (size_t i = 0; i < NUMBLETTERS; i++)
+    {
+        while (arrLetters[i].freq >= 2)
+        {
+            result = 1;
+            arrLetters[i].freq -= 2; // убираем пару
+        }
+    }
+    if (!result)
+    {
+        return 0; // нет ни одной повторяющейся буквы
+    }
+    else
+    {
+        result = 0;
+    }
+    for (size_t i = 0; i < NUMBLETTERS; i++)
+    {
+        if (arrLetters[i].freq >= 1)
+        {
+            result++; // подсчёт непарных букв
+        }
+    }
+    if (result > 1)
+        return 0;
+    else
+        return 1;
+};
+
 int main(int argv, char **argc)
 {
     FILE *fpIn, *fpOut;
     fpIn = fopen(inputFileName, "r");
     fpOut = fopen(outputFileName, "w");
-    char inpString[MAXLEN], outString[MAXLEN];
+    char inpString[MAXLEN] = {0};
     if (fpIn == NULL || fpOut == NULL) // проверка на ошибку открытия файлов
     {
         printf("Файл %s не найден! Аварийный выход из программы.\n", inputFileName);
         exit(1);
     }
     fscanf(fpIn, "%[^\n]", inpString);
-    printf("%s", inpString);
-
+    is_palindrom(inpString) ? printf("YES\n") : printf("NO\n");
     fclose(fpOut);
     fclose(fpIn);
     return 0;
