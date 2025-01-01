dbb321312b80eefe03f63dbb3ad027c99daed4f2
agvolodin <agvolodin@stdu.info>
2025-01-01 23:14:01 +0400

    CONTEST-G task G18

--- HW10/task_G18.c
+++ HW10/task_G18.c
@@ -8,25 +8,44 @@
 // Входные данные: Строка из английских букв, знаков препинания и пробелов. Не более 1000 символов.
 // Выходные данные: Строка из английских букв, знаков препинания и пробелов.
 
-#define MAXLEN 1000
+#define MAXLEN 1001
 
 const char inputFileName[] = "input.txt";
 const char outputFileName[] = "output.txt";
+
+void stripString(char *inpS, char *outS)
+{
+    int size = strlen(inpS);
+    int indexOut = 0;
+    for (size_t i = 0; i < size; i++)
+    {
+        while (i + 1 < size && inpS[i] == ' ' && inpS[i + 1] == ' ')
+        {
+            i++;
+        }
+        if (indexOut != 0 || inpS[i] != ' ')
+        {
+            outS[indexOut] = inpS[i];
+            indexOut++;
+        }
+    }
+};
 
 int main(int argv, char **argc)
 {
     FILE *fpIn, *fpOut;
     fpIn = fopen(inputFileName, "r");
     fpOut = fopen(outputFileName, "w");
-    char inpString[MAXLEN], outString[MAXLEN];
+    char inpString[MAXLEN] = {0}, outString[MAXLEN] = {0};
     if (fpIn == NULL || fpOut == NULL) // проверка на ошибку открытия файлов
     {
         printf("Файл %s не найден! Аварийный выход из программы.\n", inputFileName);
         exit(1);
     }
     fscanf(fpIn, "%[^\n]", inpString);
-    printf("%s", inpString);
-    
+    // printf("%s", inpString);
+    stripString(inpString, outString);
+    fprintf(fpOut, "%s", outString);
     fclose(fpOut);
     fclose(fpIn);
     return 0;
